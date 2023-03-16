c58da8fe2e4e96bb8c274790844479c11093ef5a
Hemant Konda <115872236+Hemantkonda@users.noreply.github.com>
2023-03-16 23:43:42 +0530

    Update main.js

--- counter/main.js
+++ counter/main.js
@@ -1,18 +1,25 @@
+//targeting all elements
 const insta = document.getElementById("insta");
 const fb = document.getElementById("fb");
 const twitter = document.getElementById("twitter");
 
+//initial value Setup
 let timer = 0;
 
-
-let count = setInterval(function(){
-    insta.innerText = timer* 4;
-    fb.innerText = timer*35;
-    twitter.innerText = timer*3;
-
-    timer++
-    if (timer == 101) {
-        window.clearInterval(count)
-    }
-},20)
+//code will run after 2 sec delay
+setTimeout(function(){
+    let count = setInterval(function(){
+        insta.innerText = timer* 4;
+        fb.innerText = timer*35;
+        twitter.innerText = timer*3;
+    
+        //timer will increase by 1
+        timer++
+        
+        //count function will stop if condition is true
+        if (timer == 101) {
+            window.clearInterval(count)
+        }
+    },10)
+},2000)
 
